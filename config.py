74e23059946e72b8a387ee782179c6c493e898a0
Siddhabrata Mohapatra <114326650+siddha-forever@users.noreply.github.com>
2024-07-30 10:02:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #open ai api key
-apikey = "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"
+apikey = "" 
 #weather api key
-w_apikey = '30d4741c779ba94c470ca1f63045390a'
+w_apikey = ""
 #news api key
-n_apikey = "773c894945b84ec188723c4ad64a1aa4"
+n_apikey = ""
